fecc493468716f2dff7c0f1d3680c10fdbb41ef2
Fredrik Leemann <fredrik@leemann.se>
2018-12-21 17:11:40 +0100

    Continued to work on bootstrap theme

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,16 +2,20 @@
 
 console.log('Hello World');
 
-const editCloseOverlayBtn = document.getElementById('edit-close-overlay-btn');
 const editToggleOverlay = document.getElementById('edit-toggle-overlay');
-const editAccountOverlayBtn = document.getElementById('edit-account');
+const secondColumnDisplay = document.getElementById('second-column');
 
+const editCloseOverlayBtn = document.getElementById('edit-close-overlay-btn');
+const editAccountOverlayBtn = document.getElementById('edit-account');
 const deleteAccountOverlayBtn = document.getElementById('delete-account');
 
 editAccountOverlayBtn.addEventListener('click', () => {
     editToggleOverlay.classList.add('overlay-display');
+    secondColumnDisplay.classList.add('d-none');
 });
 
 editCloseOverlayBtn.addEventListener('click', () => {
     editToggleOverlay.classList.remove('overlay-display');
+    secondColumnDisplay.classList.remove('d-none');
+    window.scrollTo(0, 0);
 });
