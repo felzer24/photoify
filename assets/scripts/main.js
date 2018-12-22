145dd1a941564a1ddb62666d7e469ef030934cb2
Fredrik Leemann <fredrik@leemann.se>
2018-12-22 14:32:23 +0100

    Worked on mobile navigation

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,22 +1,42 @@
 'use strict';
 
-const editToggleOverlay = document.getElementById('edit-toggle-overlay');
-const secondColumnDisplay = document.getElementById('second-column');
+// Mobile-menu
+const mobileMenu = document.querySelector('.mobile-menu');
+const wrapperFirstColumn = document.getElementById('wrapper-first-column');
+const postModuleBorderRadius = document.querySelector('.my-post-module');
 
+// Design Columns
+const firstColumn = document.getElementById('first-column');
+const secondColumn = document.getElementById('second-column');
+
+// Manage Account Overlay
+const editToggleOverlay = document.getElementById('edit-toggle-overlay');
+
+// Buttons in Manage Account Overlay
 const editCloseOverlayBtn = document.getElementById('edit-close-overlay-btn');
 const editAccountOverlayBtn = document.getElementById('edit-account');
 const deleteAccountOverlayBtn = document.getElementById('delete-account');
 
+// Prevent Enter-key in textareas
 const textAreasPrevent = document.querySelectorAll('textarea');
+
+// Code do not edit
+
+mobileMenu.addEventListener('click', () => {
+    firstColumn.classList.toggle('mb-3');
+    secondColumn.classList.remove('d-none');
+    wrapperFirstColumn.classList.toggle('d-block');
+    postModuleBorderRadius.classList.toggle('border-top__toggle');
+});
 
 editAccountOverlayBtn.addEventListener('click', () => {
     editToggleOverlay.classList.add('overlay-display');
-    secondColumnDisplay.classList.add('d-none');
+    secondColumn.classList.add('d-none');
 });
 
 editCloseOverlayBtn.addEventListener('click', () => {
     editToggleOverlay.classList.remove('overlay-display');
-    secondColumnDisplay.classList.remove('d-none');
+    secondColumn.classList.remove('d-none');
     window.scrollTo(0, 0);
 });
 
