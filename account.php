364c33ba3f51a71549182cb5071db4e0459c9803
Fredrik Leemann <fredrik@leemann.se>
2018-12-26 20:22:49 +0100

    Deleted some files and programed redirects if not logged in on account.php

--- account.php
+++ account.php
@@ -1,5 +1,11 @@
-<?php require __DIR__.'/views/header.php'; ?>
+<?php
+    require __DIR__.'/views/header.php';
 
+    if (!isset($_SESSION['logedin'])) {
+        redirect('/');
+        exit();
+    }
+?>
     <main data-errors="<?= $_SESSION['page']['error'] ?? 'no' ?>">
         <!-- Start main container -->
         <div class="container">
--- views/footer.php
+++ views/footer.php
@@ -1,14 +1,11 @@
     <script src="/assets/scripts/main.js"></script>
 
-    <footer>
-
-    </footer>
-
     <?php
         unset($_SESSION['page']);
         unset($_SESSION['errors']);
         unset($_SESSION['values']);
         unset($_SESSION['banner']);
     ?>
+
 </body>
 </html>
